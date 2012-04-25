c3fbf821691a1adaa26e64d45ede411d7a140948
Michael Pujos <bubbleguuum@free.fr>
2012-04-25 13:58:22 +0200

    catch IllegalArgumentException thrown by Apache HttpClient on blank URI

--- core/src/main/java/org/fourthline/cling/protocol/RetrieveRemoteDescriptors.java
+++ core/src/main/java/org/fourthline/cling/protocol/RetrieveRemoteDescriptors.java
@@ -119,18 +119,23 @@ public class RetrieveRemoteDescriptors implements Runnable {
     	}
     	
     	StreamRequestMessage deviceDescRetrievalMsg;
+    	StreamResponseMessage deviceDescMsg;
 
     	try {
+    		
     		deviceDescRetrievalMsg =
                 new StreamRequestMessage(UpnpRequest.Method.GET, rd.getIdentity().getDescriptorURL());
+
+    		log.fine("Sending device descriptor retrieval message: " + deviceDescRetrievalMsg);
+            deviceDescMsg = getUpnpService().getRouter().send(deviceDescRetrievalMsg);
+            
     	} catch(IllegalArgumentException e) {
     		// UpnpRequest constructor can throw IllegalArgumentException on invalid URI
+    		// IllegalArgumentException can also be thrown by Appache HttpClient on blank URI in send()
             log.warning("Device descriptor retrieval failed: " + e.getMessage());
             return ;
     	}
 
-        log.fine("Sending device descriptor retrieval message: " + deviceDescRetrievalMsg);
-        StreamResponseMessage deviceDescMsg = getUpnpService().getRouter().send(deviceDescRetrievalMsg);
 
         if (deviceDescMsg == null) {
             log.warning("Device descriptor retrieval failed, no response: " + rd.getIdentity().getDescriptorURL());
