1f0a6472be88ccf82e7827da6ef4a29339ed3cf0
Sebastian Roth <sebastian.roth@gmail.com>
2015-03-18 17:40:54 +0800

    Temporary fix to allow for custom Device Token Name & Version

--- core/src/main/java/org/fourthline/cling/model/UserConstants.java
+++ core/src/main/java/org/fourthline/cling/model/UserConstants.java
@@ -22,9 +22,9 @@ package org.fourthline.cling.model;
  */
 public class UserConstants {
 
-    public static final String PRODUCT_TOKEN_NAME = "Cling";
+    public static String PRODUCT_TOKEN_NAME = "Cling";
 
-    public static final String PRODUCT_TOKEN_VERSION = "2.0";
+    public static String PRODUCT_TOKEN_VERSION = "2.0";
 
     public static final int DEFAULT_SUBSCRIPTION_DURATION_SECONDS = 1800;
 
